771a99d714c88f4a343e5b8cba859a8dcc1f153d
r.tyminski <r.tyminski@partner.samsung.com>
2019-01-03 12:03:39 +0100

    Fix cross compilation.

--- compile.sh
+++ compile.sh
@@ -3,15 +3,16 @@
 CURR_PWD=$PWD
 LIB_DIR=$PWD/libs
 BUILD_PATH=build
-TOOLCHAIN_FILE=toolchain.cmake
-SYSROOT=$CURR_PWD/$BUILD_PATH/rootfs
+TOOLCHAIN_FILE=$PWD/toolchain.cmake
+DESTDIR=$CURR_PWD/$BUILD_PATH/rootfs
 COMPILER_VERSION=$(gcc -v 2>&1 | grep "gcc version" | cut -c 13-17)
 HOST=
 if [ "$COMPILER_VERSION" \> "7.0.0" ]; then
     echo "Host compiler is greater version than 7. It supports C++17. There's no need to use toolchain."
 else
     echo "Host compiler version is $COMPILER_VERSION. It doesn't supports C++17. Try to use toolchain."
-    CROSS_COMPILE=$RT_OPENSRC_PATH/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
+    CROSS_COMPILE_PATH=$RT_OPENSRC_PATH/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi/
+    CROSS_COMPILE=$CROSS_COMPILE_PATH/bin/arm-linux-gnueabi-
     HOST=arm-linux-gnueabi
 fi
 
@@ -27,7 +28,7 @@ fi
 
 cd $BUILD_PATH/zlib
 if [ ! -f Makefile ]; then
-    CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar STRIP=${CROSS_COMPILE}strip RANLIB=${CROSS_COMPILE}ranlib $LIB_DIR/zlib/configure --prefix=$CURR_PWD/$BUILD_PATH/rootfs
+    CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar STRIP=${CROSS_COMPILE}strip RANLIB=${CROSS_COMPILE}ranlib $LIB_DIR/zlib/configure --prefix=$DESTDIR
 fi
 make
 make install
@@ -35,12 +36,12 @@ cd ../
 
 cd libpng
 if [ ! -f Makefile ]; then
-    $LIB_DIR/libpng/configure --prefix=$CURR_PWD/$BUILD_PATH/rootfs --host=$HOST CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar STRIP=${CROSS_COMPILE}strip RANLIB=${CROSS_COMPILE}ranlib CPPFLAGS="-I$CURR_PWD/$BUILD_PATH/rootfs/include" LDFLAGS="-L$CURR_PWD/$BUILD_PATH/rootfs/lib/"
+    $LIB_DIR/libpng/configure --prefix=$DESTDIR --host=$HOST CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar STRIP=${CROSS_COMPILE}strip RANLIB=${CROSS_COMPILE}ranlib CPPFLAGS="-I$DESTDIR/include" LDFLAGS="-L$DESTDIR/lib/"
 fi
 make
 make install
 cd ../
 
-cmake ../ -DCROSS_COMPILE=$CROSS_COMPILE -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -DROOTFS=$CURR_PWD/$BUILD_PATH/rootfs/
+cmake ../ -DCROSS_COMPILE=$CROSS_COMPILE -DCROSS_COMPILE_ROOT=$CROSS_COMPILE_PATH -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -DDESTDIR=$DESTDIR
 make
-cd -
+make DESTDIR=$DESTDIR install
