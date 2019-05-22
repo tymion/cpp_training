58d8d674205ef7776c2b5f12cb1452380b6f04f8
Rafał Tymiński <r.tyminski@partner.samsung.com>
2019-05-22 16:17:07 +0200

    Dynamic size free list implementation

--- include/dynamic_size_free_list.h
+++ include/dynamic_size_free_list.h
@@ -5,9 +5,78 @@
 
 class FreeList : NullPtrAllocator {
     A _pool;
-    struct Node { Node * next; } *_root = nullptr;
+    struct Node {
+        Node * next;
+        Node * prev;
+        size_t size;
+    } *_root = nullptr;
 
     private:
+        Node * getNode(size_t size) {
+            Node *tmp = _root;
+            while (tmp != nullptr) {
+                if (tmp.size >= size) {
+                    return tmp;
+                }
+                tmp = tmp->next;
+            }
+            return tmp;
+        }
+
+        void insertNode(Blk& mem) {
+            Node *iterator = _root;
+            Node *tmp = _root;
+            if (_root == NULL) {
+                _root = (Node *) mem.ptr;
+                _root->prev = nullptr;
+                _root->next = nullptr;
+                _root->size = mem.size;
+                return;
+            }
+            do {
+                if (iterator == mem.ptr + mem.size) {
+                    Node *ltmp = (Node *) mem.ptr;
+                    if (iterator->prev) {
+                        iterator->prev->next = ltmp;
+                    } else {
+                        _root = ltmp;
+                    }
+                    if (iterator->next) {
+                        iterator->next->prev = ltmp;
+                    }
+                    ltmp->prev = iterator->prev;
+                    ltmp->next = iterator->next;
+                    ltmp->size = iterator->size + mem.size;
+                    return;
+                }
+                if (iterator + iterator.size == mem.ptr) {
+                    iterator->size = iterator->size + mem.size;
+                    return;
+                }
+                if (!tmp && iterator->size > mem.ptr) {
+                    tmp = iterator;
+                }
+            } while (iterator->next != NULL && iterator = iterator->next)
+            // If we end up here that means we didn't merge regions
+            if (tmp) {
+                // We put region between others (sorted)
+                Node *ltmp = (Node *) mem.ptr;
+                if (tmp->prev) {
+                    tmp->prev->next = ltmp;
+                } else {
+                    _root = ltmp;
+                }
+                if (tmp->next) {
+                    tmp->next->prev = ltmp;
+                }
+                ltmp->prev = tmp->prev;
+                ltmp->next = tmp->next;
+                ltmp->size = tmp->size + mem.size;
+            } else {
+                // We put region at the end
+                
+            }
+        }
 
     public:
         void deallocate(Blk& mem)
