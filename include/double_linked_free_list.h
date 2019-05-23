a0e20a2bac79dc58e7a573912bcc7ba5f426cffd
Rafał Tymiński <tyminski.rafal@gmail.com>
2019-05-23 21:42:06 +0200

    Small fixes

--- include/double_linked_free_list.h
+++ include/double_linked_free_list.h
@@ -2,18 +2,20 @@
 
 #include <cstddef>
 
-typedef struct Node_ {
+typedef struct Node_ Node;
+
+struct Node_ {
     Node * next;
     Node * prev;
     size_t size;
-} Node;
+};
 
 class DoubleLinkedFreeList
 {
 
     private:
         Node *_root = nullptr;
-	Node *_last = nullptr;
+        Node *_last = nullptr;
         void beforeNode(Node *node, Node *ptr);
         void insertBeforeNode(Node *node, Node *ptr);
         void insertNodeAfterLast(Node* last, Node* ptr);
